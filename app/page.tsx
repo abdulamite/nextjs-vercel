ba28f2d487522fd6700ae9b70de0fe5216378f08
Abdul Alberto-Shaikh <abdulamite@gmail.com>
2024-03-22 01:33:57 -0700

    Resolve post sort issue

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@ import { allPosts } from "@/.contentlayer/generated"
 import Link from "next/link"
 
 export default function Home() {
+  const posts = allPosts.sort((a, b) => {
+    return new Date(a.date).getTime() - new Date(b.date).getTime() 
+  })
   return (
     <div className="prose dark:prose-invert">
-      {allPosts.map((post) => (
+      {posts.map((post) => (
         <article key={post._id}>
           <Link href={post.slug}>
             <h2>{post.title}</h2>
